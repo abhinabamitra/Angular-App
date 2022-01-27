d75c57ef88a3f073697777f4c4bbb10fc7c34fd5
troj4n <abhinabamitra19@gmail.com>
2022-01-27 21:44:26 +0530

    Add Team Generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-App';
+  //title = 'Angular-App';
+  newMemberName = "";
+  members:string[] = [];
+  errorMessage ='';
+  numberofTeams:number | "" = "";
+
+  addmember(){
+    //console.log('CLICKED')
+    if(!this.newMemberName){
+      this.errorMessage = "Name can't be empty"
+      return;
+    }
+    this.members.push(this.newMemberName);
+    this.newMemberName ="";
+    this.errorMessage ="";
+    console.log(this.members);
+  }
+
+  onInput(member:string) {
+    this.newMemberName = member;
+    //console.log(this.newMemberName)
+  }
+
+  onNumberOfTeamsInput(value:string) {
+    this.numberofTeams = Number(value);
+
+  }
+
+  generateTeams() {
+    if(!this.numberofTeams || this.numberofTeams<=0){
+      return;
+    }
+
+    const allMembers = [...this.members]
+
+    for(let i=0; i< this.numberofTeams; i++) {
+      const randomIndex = Math.floor(Math.random()*allMembers.length)
+      const member = allMembers.splice(randomIndex,1)[0];
+    }
+  }
+
 }
